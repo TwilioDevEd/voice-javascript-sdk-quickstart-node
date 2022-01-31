ac80e032418676bdd6bfb1fb78b4a52d0ee8686c
Brianna DelValle <Briannadelvalle@gmail.com>
2022-01-31 15:03:53 -0700

    Remove outgoing call 'reject' event listener

--- public/quickstart.js
+++ public/quickstart.js
@@ -118,7 +118,6 @@
       call.on("accept", updateUIAcceptedOutgoingCall);
       call.on("disconnect", updateUIDisconnectedOutgoingCall);
       call.on("cancel", updateUIDisconnectedOutgoingCall);
-      call.on("reject", updateUIDisconnectedOutgoingCall);
 
       outgoingCallHangupButton.onclick = () => {
         log("Hanging up ...");
